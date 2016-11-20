fbbcf00adafab8cdf30077df7a4abbf4fd4b4b90
Chris Rabuse <rab233@live.com>
2016-11-19 23:06:04 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string name = "Chris";
+            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine($"The current time is {DateTime.Now}");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
